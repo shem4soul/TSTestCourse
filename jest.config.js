db529e8ae2048529d91165548d2843c71138ce72
Shittu Emmanuel <shem4soul@gmail.com>
2026-03-01 16:33:57 +0100

    chore: migrate Jest configuration from TypeScript to JavaScript

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+};
+// import type { Config } from "@jest/types";
+
+// const config: Config.InitialOptions = {
+//   preset: "ts-jest",
+//   testEnvironment: "node",
+//   verbose: true,
+// };
+
+// export default config;
